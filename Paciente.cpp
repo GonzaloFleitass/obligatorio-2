96de6f46432d04a48b3f91f8e1822a1b6711a631
miguelescanocedres <114918783+miguelescanocedres@users.noreply.github.com>
2023-11-16 18:59:44 -0300

    Add files via upload

---
+++ Paciente.cpp
@@ -0,0 +1,23 @@
+//
+//  Paciente.cpp
+//  ObligatorioFinal
+//
+//  Created by Miguel Lescano on 15/11/23.
+//
+
+#include <stdio.h>
+#include "Paciente.h"
+
+
+void cargarPaciente (Paciente &p){
+    printf("Ingrese la cedula : ");
+    scanf("%ld", &p.cedula);
+    printf("Ingrese el nombre: ");
+    scan(p.nombre);
+    printf("Ingrese el apellido: ");
+    scan(p.apellido);
+    printf("Ingrese el telefono: ");
+    scanf("%ld", &p.telefono);
+}
+
+printf("hola");
