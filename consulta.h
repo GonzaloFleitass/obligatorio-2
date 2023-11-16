beb4b4007ec0962416c364207fff6d51f52ed3eb
GonzaloFleitass <144866796+GonzaloFleitass@users.noreply.github.com>
2023-11-15 21:10:37 -0300

    Add files via upload

---
+++ consulta.h
@@ -0,0 +1,24 @@
+//
+//  consulta.h
+//  Obligatorio 2
+//
+//  Created by Gonzalo Fleitas on 15/11/23.
+//
+
+#ifndef consulta_h
+#define consulta_h
+typedef struct {
+    Fecha Fech;
+    string motivoConsulta;
+    long int ci;
+    evaluacion eva;
+}consulta;
+
+
+void cargarConsulta(consulta &a);/*carga todos los datos de la consulta*/
+void mostrarConsulta(consulta a); /*muestra todos los datos de consulta*/
+Fecha devolverFechConsulta(consulta a);/*devuelve el valor de fecha adentro de consulta*/
+void devolverMotivoConsulta(consulta b,string &a);/*devuelve el valor de motivo adentro de consulta*/
+long int devolverCiConsulta(consulta b);/*devuelve el valor de la cedula */
+evaluacion devolverEvaluacionConsulta(consulta b);/*devuelve el valor de la evaluacion dentro de consulta*/
+#endif /* consulta_h */
