4228a3e74c5faf131bdbdc21bb4acaa38cc04373
rominacaballero4 <150977572+rominacaballero4@users.noreply.github.com>
2023-11-17 19:12:22 -0300

    submenuconsultas.cpp

--- submenuconsultas.cpp
+++ submenuconsultas.cpp
@@ -2,27 +2,23 @@
 
 void submenuconsultas(int &opcionsub2){
 
-    printf("*-----------------------------------------------------------*\n");
-    printf("|                      Consultas                            |\n");
-    printf("|                                                           |\n");
-    printf("|      1. Registrar nueva consulta                          |\n");
-    printf("|-----------------------------------------------------------|\n");
-    printf("|      2. Listar todas las consultas de un paciente (Cedula)|\n");
-    printf("|-----------------------------------------------------------|\n");
-    printf("|      3. Listar todas las consultas (Fecha)          |\n");
-    printf("|-------------------------------------                    |\n");
-    printf("|      4. Buscar pacientes con X cantidad de consultas     |\n");
-    printf("|                                                         |\n");
-    printf("|-------------------------------------                    |\n");
-    printf("|      5. Obtener Cantidad de Consultas de cada tipo                     |\n");
-    printf("|                                                         |\n");
-    printf("*-------------------------------------                    *\n");
-    printf("|      6. Obtener cantidad de consultas en X dia                    |\n");
-    printf("|                                                         |\n");
-    printf("*-------------------------------------                    *\n");
-    printf("|      7. Obtener el paciente con la mayor cantidad de consultas                    |\n");
-    printf("|                                                         |\n");
-    printf("*-------------------------------------                    *\n");
+    printf("*---------------------------------------------------------------*\n");
+    printf("|                      Consultas                                |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("|      1. Registrar nueva consulta                              |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("|      2. Listar todas las consultas de un paciente (Cedula)    |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("|      3. Listar todas las consultas (Fecha)                    |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("|      4. Buscar pacientes con X cantidad de consultas          |\n");
+    printf("|---------------------------------------------------------------|\n");
+    printf("|      5. Obtener Cantidad de Consultas de cada tipo            |\n");
+    printf("*---------------------------------------------------------------|\n");
+    printf("|      6. Obtener cantidad de consultas en X dia                |\n");
+    printf("*---------------------------------------------------------------|\n");
+    printf("|      7. Obtener el paciente con la mayor cantidad de consulta |\n");
+    printf("*---------------------------------------------------------------*\n");
 
 
     printf("Ingrese su opcion: ");
