a48ff07e5f388c98a5bfa8fa2e7cc9d5c6b3cffa
rominacaballero4 <150977572+rominacaballero4@users.noreply.github.com>
2023-11-17 19:13:19 -0300

    menualtasbajas.cpp

--- menualtasbajas.cpp
+++ menualtasbajas.cpp
@@ -3,8 +3,8 @@
 
 void submenualtasybajas (int &opcionsub1){
     printf("*---------------------------------------------------------*\n");
-    printf("|                         Pacientes                       |\n");
-    printf("|                                                         |\n");
+    printf("|                     Pacientes                           |\n");
+    printf("|---------------------------------------------------------|\n");
     printf("|      1. Registrar nuevo paciente                        |\n");
     printf("|---------------------------------------------------------|\n");
     printf("|      2. Eliminar Paciente Existente                     |\n");
@@ -12,10 +12,8 @@ void submenualtasybajas (int &opcionsub1){
     printf("|      3. Listar todos los pacientes registrados          |\n");
     printf("|---------------------------------------------------------|\n");
     printf("|      4. Listar pacientes sin consultas registradas      |\n");
-    printf("|                                                         |\n");
     printf("|---------------------------------------------------------|\n");
     printf("|      5. Volver al menu principal                        |\n");
-    printf("|                                                         |\n");
     printf("*---------------------------------------------------------*\n");
 
 
