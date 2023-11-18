a14e3667bae568d439002f587247806bc17e2db1
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2023-11-17 22:41:24 -0300

    subido

--- obligatorio mac/obligatorio mac/main.cpp
+++ obligatorio mac/obligatorio mac/main.cpp
@@ -14,30 +14,36 @@ int main() {
             switch(opcionsub1){
                 case 1:printf("Ingrese CI:");
                     scanf("%ld", &ci);
-                    if(Pertenece(a, ci))
+                    if(Pertenece(a, ci)){
                         printf("El paciente ya existe");
-                    else{
+                    } else{
                         cargarPaciente(p);
                         Insert(a, p);
                     }
                 case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+            
+            break;
+        case 2: submenuconsultas(opcionsub2);
+            switch(opcionsub2){
+                case 1:
+                case 2:
                 case 3:
                 case 4:
                 case 5:
+                case 6:
+                case 7:
+                    break;
+                    
             }
-    
-    break;
-case 2: submenuconsultas(opcionsub2);
-    switch(opcionsub2){
-        case 1:
-        case 2:
         case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-            
+            break;
     }
-case 3:
-}
 }
