94cf486f0d429bc7f46fb51a66e3f174a99927e2
GonzaloFleitass <144866796+GonzaloFleitass@users.noreply.github.com>
2023-11-17 21:08:56 -0300

    Add files via upload

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,10 +1,38 @@
-#include <stdio.h>
-#include "arbolpacientes.h"
+//
+//  main.cpp
+//  Obligatorio 2
+//
+//  Created by Gonzalo Fleitas on 15/11/23.
+//
 
-using namespace std;
+#include "menu.h"
 
-int main()
-{
 
-    return 0;
+int main() {  
+mostarmenu1(int opcion);
+switch(opcion){
+   case 1:submenualtasybajas (int &pcionsub1);
+        switch(opcionsub1){
+                int ci;
+            case 1:printf("Ingrese CI:");
+                    scanf("%d", &ci)
+                perteneceCI
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        }
+   break;
+   case 2: submenuconsultas(int opcionsub2);
+   switch(opcionsub2){
+       case 1:
+       case 2:
+       case 3:
+       case 4:
+       case 5:
+       case 6:
+       case 7:
+           
+}
+case 3:
 }
