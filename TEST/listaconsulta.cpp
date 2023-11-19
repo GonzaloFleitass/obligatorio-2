4a5791cff3bde6c370064013c2c85a2fa5d603b7
rominacaballero4 <150977572+rominacaballero4@users.noreply.github.com>
2023-11-19 16:22:50 -0300

    listaconsulta.cpp

--- TEST/listaconsulta.cpp
+++ TEST/listaconsulta.cpp
@@ -1,1 +1,19 @@
-#include "consulta.h"
+#include "listaconsulta.h"
+
+void Crear (Lista &l){
+    l=NULL;
+}
+
+void InsFrontConsulta (Lista &l, consulta c){
+    Lista aux = new Nodo;
+    aux->con=c;
+    aux->sig=l;
+    l=aux;
+}
+
+void DesplegarLista(Lista l){
+    while(l!=NULL){
+        mostrarConsulta(l->con);
+        l= l->sig;
+    }
+}
