e3c981f84fc00879bd58baaf4c852fb4b7f5d030
rominacaballero4 <150977572+rominacaballero4@users.noreply.github.com>
2023-11-19 20:44:16 -0300

    menu.cpp

--- obligatorio mac/obligatorio mac/menu.cpp
+++ obligatorio mac/obligatorio mac/menu.cpp
@@ -4,15 +4,15 @@
 void mostarmenu1(int &opcion){
 
 
-    printf("*-------------------------------------*\n");
-    printf("|           MENU PRINCIPAL            |\n");
-    printf("|-------------------------------------|\n");
-    printf("|         1. Pacientes                |\n");
-    printf("|-------------------------------------|\n");
-    printf("|         2. Consultas                |\n");
-    printf("|-------------------------------------|\n");
-    printf("|         3. Salir del programa       |\n");
-    printf("*-------------------------------------*\n");
+    printf("\t\t\t\t\t*-------------------------------------*\n");
+    printf("\t\t\t\t\t|           MENU PRINCIPAL            |\n");
+    printf("\t\t\t\t\t|-------------------------------------|\n");
+    printf("\t\t\t\t\t|         1. Pacientes                |\n");
+    printf("\t\t\t\t\t|-------------------------------------|\n");
+    printf("\t\t\t\t\t|         2. Consultas                |\n");
+    printf("\t\t\t\t\t|-------------------------------------|\n");
+    printf("\t\t\t\t\t|         3. Salir del programa       |\n");
+    printf("\t\t\t\t\t*-------------------------------------*\n");
 
     printf("Ingrese su opcion: ");
     scanf("%d", &opcion);
@@ -20,19 +20,19 @@ void mostarmenu1(int &opcion){
 }
 
 void submenualtasybajas (int &opcionsub1){
-    printf("*---------------------------------------------------------*\n");
-    printf("|                     Pacientes                           |\n");
-    printf("|---------------------------------------------------------|\n");
-    printf("|      1. Registrar nuevo paciente                        |\n");
-    printf("|---------------------------------------------------------|\n");
-    printf("|      2. Eliminar Paciente Existente                     |\n");
-    printf("|---------------------------------------------------------|\n");
-    printf("|      3. Listar todos los pacientes registrados          |\n");
-    printf("|---------------------------------------------------------|\n");
-    printf("|      4. Listar pacientes sin consultas registradas      |\n");
-    printf("|---------------------------------------------------------|\n");
-    printf("|      5. Volver al menu principal                        |\n");
-    printf("*---------------------------------------------------------*\n");
+    printf("\t\t\t\t*---------------------------------------------------------*\n");
+    printf("\t\t\t\t|                     Pacientes                           |\n");
+    printf("\t\t\t\t|---------------------------------------------------------|\n");
+    printf("\t\t\t\t|      1. Registrar nuevo paciente                        |\n");
+    printf("\t\t\t\t|---------------------------------------------------------|\n");
+    printf("\t\t\t\t|      2. Eliminar Paciente Existente                     |\n");
+    printf("\t\t\t\t|---------------------------------------------------------|\n");
+    printf("\t\t\t\t|      3. Listar todos los pacientes registrados          |\n");
+    printf("\t\t\t\t|---------------------------------------------------------|\n");
+    printf("\t\t\t\t|      4. Listar pacientes sin consultas registradas      |\n");
+    printf("\t\t\t\t|---------------------------------------------------------|\n");
+    printf("\t\t\t\t|      5. Volver al menu principal                        |\n");
+    printf("\t\t\t\t*---------------------------------------------------------*\n");
 
 
     printf("Ingrese su opcion: ");
@@ -42,23 +42,25 @@ void submenualtasybajas (int &opcionsub1){
 
     void submenuconsultas(int &opcionsub2){
 
-    printf("*---------------------------------------------------------------*\n");
-    printf("|                      Consultas                                |\n");
-    printf("|---------------------------------------------------------------|\n");
-    printf("|      1. Registrar nueva consulta                              |\n");
-    printf("|---------------------------------------------------------------|\n");
-    printf("|      2. Listar todas las consultas de un paciente (Cedula)    |\n");
-    printf("|---------------------------------------------------------------|\n");
-    printf("|      3. Listar todas las consultas (Fecha)                    |\n");
-    printf("|---------------------------------------------------------------|\n");
-    printf("|      4. Buscar pacientes con X cantidad de consultas          |\n");
-    printf("|---------------------------------------------------------------|\n");
-    printf("|      5. Obtener Cantidad de Consultas de cada tipo            |\n");
-    printf("*---------------------------------------------------------------|\n");
-    printf("|      6. Obtener cantidad de consultas en X dia                |\n");
-    printf("*---------------------------------------------------------------|\n");
-    printf("|      7. Obtener el paciente con la mayor cantidad de consulta |\n");
-    printf("*---------------------------------------------------------------*\n");
+    printf("\t\t\t\t*---------------------------------------------------------------*\n");
+    printf("\t\t\t\t|                      Consultas                                |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      1. Registrar nueva consulta                              |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      2. Listar todas las consultas de un paciente (Cedula)    |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      3. Listar todas las consultas (Fecha)                    |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      4. Buscar pacientes con X cantidad de consultas          |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      5. Obtener Cantidad de Consultas de cada tipo            |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      6. Obtener cantidad de consultas en X dia                |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      7. Obtener el paciente con la mayor cantidad de consulta |\n");
+    printf("\t\t\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t\t\t|      8. Volver al menu principal                              |\n");
+    printf("\t\t\t\t*---------------------------------------------------------------*\n");
 
 
     printf("Ingrese su opcion: ");
