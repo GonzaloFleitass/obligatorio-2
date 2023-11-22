8f3d50ed5a4bef6f8381f7674bfdce9740092a2f
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2023-11-22 20:26:37 -0300

    hola

--- obligatorio mac/obligatorio mac/main.cpp
+++ obligatorio mac/obligatorio mac/main.cpp
@@ -11,16 +11,17 @@ int main() {
     ArboldePacientes a;
     Paciente p;
     consulta c;
-
+    
     Fecha fech,fech2, fech3;
     Lista consultas;
     Crear(consultas);
     Crear(a);
-
+    
     do{
         system("cls");
         mostarmenu1( opcion);
-
+        int cantFech = 0, cantConsu;
+        long int ciBusca;
         switch(opcion){
             case 1:
                 system("cls");
@@ -46,28 +47,27 @@ int main() {
                             printf("El paciente no existe\n");
                         }
                         system("pause");
-                            break;
-                        case 3:
-                            if(EsVacio(a))
-                                printf("No existen pacientes\n");
-                            else
-                                printOrden(a);
-                           system("pause");
-                            break;
-                        case 4:sinConsultas (a);
-                            system("pause");
-                            break;
-                        case 5:
-                            break;
-                        }
-
                         break;
-
-                case 2:
-                    system("cls");
-                    submenuconsultas(opcionsub2);
+                    case 3:
+                        if(EsVacio(a))
+                            printf("No existen pacientes\n");
+                        else
+                            printOrden(a);
+                        system("pause");
+                        break;
+                    case 4:sinConsultas (a);
+                        system("pause");
+                        break;
+                    case 5:
+                        break;
+                }
+                
+                break;
+                
+            case 2:
+                system("cls");
+                submenuconsultas(opcionsub2);
                 switch(opcionsub2){
-                        
                     case 1:
                         printf("Ingrese CI: ");
                         scanf("%ld",&ci2);
@@ -93,48 +93,47 @@ int main() {
                                 }
                             }
                         }
-                
-                                system("pause");
-                                break;
-                            case 2:long int ciBusca;
-                                printf("ingrese Ci a buscar:");
-                                scanf("%ld",&ciBusca);
-                                mostrarConsultasCI(consultas,ciBusca);
-                                system("pause");
-                                break;
-                            case 3:printf("Ingrese Fecha");
-                                CargaFech(fech);
-                                DesplegarListaFecha(consultas,fech);
-                                system("pause");
-                                break;
-                            case 4:int cantConsu;
-                                printf("Ingrese cantidad de consultas:");
-                                scanf("%d",&cantConsu);
-                                buscarPacienteXcantConsu(a,cantConsu);
-                               system("pause");
-                                break;
-                            case 5:
-                                contadorTiposConsulta(consultas,enTratamiento,saludable,necesitaProtesis);
-                                printf("Cantidad de Consulta del Tipo En Tratmiento es:%d\n",enTratamiento);
-                                printf("Cantidad de Consultas del Tipo Necesita Protesis:%d\n",necesitaProtesis);
-                                printf("Cantidad de Consultas del Tipo Saludable:%d\n",saludable);
-                                system("pause");
-                                break;
-                            case 6:int cantFech = 0;
-                                CargaFech(fech3);
-                                cantConsultasFecha(consultas, fech3);
-                                break;
-                            case 7: 
-                                break;
-
-                            case 8:
-                                break;
-
-                        }
+                        
+                        system("pause");
                         break;
-                case 3: termine=TRUE;
+                    case 2:
+                        printf("ingrese Ci a buscar:");
+                        scanf("%ld",&ciBusca);
+                        mostrarConsultasCI(consultas,ciBusca);
+                        system("pause");
+                        break;
+                    case 3:printf("Ingrese Fecha");
+                        CargaFech(fech);
+                        DesplegarListaFecha(consultas,fech);
+                        system("pause");
+                        break;
+                    case 4:
+                        printf("Ingrese cantidad de consultas:");
+                        scanf("%d",&cantConsu);
+                        buscarPacienteXcantConsu(a,cantConsu);
+                        system("pause");
+                        break;
+                    case 5:
+                        contadorTiposConsulta(consultas,enTratamiento,saludable,necesitaProtesis);
+                        printf("Cantidad de Consulta del Tipo En Tratmiento es:%d\n",enTratamiento);
+                        printf("Cantidad de Consultas del Tipo Necesita Protesis:%d\n",necesitaProtesis);
+                        printf("Cantidad de Consultas del Tipo Saludable:%d\n",saludable);
+                        system("pause");
+                        break;
+                    case 6:
+                        cantFech = 0;
+                        CargaFech(fech3);
+                        cantConsultasFecha(consultas, fech3);
+                        break;
+                    case 7:
+                        break;
+                        
+                    case 8:
                         break;
                 }
-        }while(termine!=TRUE);
+            case 3: termine=TRUE;
+                break;
+        }
+}   while(termine!=TRUE);
     return 0;
 }
