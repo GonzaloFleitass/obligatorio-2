937392b36862a5619537fe4c8b53f9e0e89ef502
GonzaloFleitass <gonzalofleitasuy@gmail.com>
2023-11-22 18:30:44 -0300

    guardar

--- obligatorio mac/obligatorio mac/listaconsulta.cpp
+++ obligatorio mac/obligatorio mac/listaconsulta.cpp
@@ -49,7 +49,7 @@ void contadorTiposConsulta(Lista z,int &a,int &b,int &c){
     b=0;
     c=0;
     while(z!=NULL){
-        printf("antes de sumar %d %d %d",a,b,c);
+       
         switch(devolverEvaluacionConsulta(z->con)){
             case EN_TRATAMIENTO: a++;
                 break;
@@ -58,7 +58,6 @@ void contadorTiposConsulta(Lista z,int &a,int &b,int &c){
             case NECESITA_PROTESIS: c++;
                 break;
         }
-        printf("despues de sumar %d %d %d",a,b,c);
         
         z=z->sig;
     }
